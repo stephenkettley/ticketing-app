fdc5442085871d360f5c3f9172ed9b4482107173
Stephen Kettley <kettleystephen@gmail.com>
2023-12-18 10:58:50 +0200

    ticket database rendering

--- app/api/Tickets/route.js
+++ app/api/Tickets/route.js
@@ -12,3 +12,13 @@ export async function POST(req) {
     return NextResponse.json({ message: "Error", error }, { status: 500 });
   }
 }
+
+export async function GET() {
+  try {
+    const tickets = await Ticket.find();
+    return NextResponse.json({ tickets }, { status: 200 });
+    return;
+  } catch (error) {
+    return NextResponse.json({ message: "Error", error }, { status: 500 });
+  }
+}
