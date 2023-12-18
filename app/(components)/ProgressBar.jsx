9df20471e0b9529c48eac8deb46297a5cb10825a
Stephen Kettley <kettleystephen@gmail.com>
2023-12-18 10:58:11 +0200

    dynamically render ticket info

--- app/(components)/ProgressBar.jsx
+++ app/(components)/ProgressBar.jsx
@@ -1,7 +1,10 @@
-const ProgressBar = () => {
+const ProgressBar = ({ progress }) => {
   return (
     <div className="w-full bg-gray-200 rounded-full h-2.5 ">
-      <div className="bg-nav h-2.5 rounded-full" style={{ width: "75%" }}></div>
+      <div
+        className="bg-nav h-2.5 rounded-full"
+        style={{ width: `${progress}%` }}
+      ></div>
     </div>
   );
 };
